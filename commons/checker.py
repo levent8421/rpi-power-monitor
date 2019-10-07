eb14f8ee49f1e88d7f842688dc976dc7d3669520
levent <levent8421@outlook.com>
2019-10-07 18:42:09 +0800

    checker

--- commons/checker.py
+++ commons/checker.py
@@ -7,5 +7,7 @@ def no_none(param, ex, msg):
         if isinstance(param, str):
             if param.isspace():
                 __raise_exception(ex, msg)
+        elif len(param) <= 0:
+            __raise_exception(ex, msg)
     else:
         __raise_exception(ex, msg)
